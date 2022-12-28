78939a4fc540c45bb6305beebc5bf0eb128f74eb
bruhulance <liam.major91@gmail.com>
2022-12-27 20:08:52 -0500

    added some boilerplate HTTP get code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use tokio;
+use regex::Regex;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Some simple CLI args requirements...
+    let url = String::from("https://hyper.rs");
+
+    println!("Fetching {:?}...", url);
+
+    // reqwest::get() is a convenience function.
+    // In most cases, you should create/build a reqwest::Client and reuse
+    // it for all requests.
+    let res = reqwest::get(url).await?;
+
+    println!("Response: {:?} {}", res.version(), res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    let body = res.text().await?;
+
+    println!("{}", body);
+
+    Ok(())
 }
 
