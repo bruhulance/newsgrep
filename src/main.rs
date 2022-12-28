9e923ec61e77b50c1f2af56ddc8c9f1abc775acf
bruhulance <liam.major91@gmail.com>
2022-12-28 17:16:35 -0500

    further continued abstraction, added more functions to work with scraping anchor tags from the HTML response

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,25 @@ use website::Website;
 
 #[tokio::main]
 async fn main() {
-    let mut site = Website::new();
-    site.url = String::from("https://hyper.rs");
+    const URL: &str = "https://www.nytimes.com/ca/";
+    let site = Website::with_url_and_sel(String::from(URL), String::from("section.story-wrapper > a"));
 
-    site.make_get().await;
+    // matching the return type of the response
+    let res = match site.make_get().await {
+        Ok(dump) => dump,
+        Err(error) => {
+            eprintln!("There has been an error: {:?}", error);
+            String::new()
+        }
+    };
+
+    // parsing the response to find articles
+    match &site.find_articles(&res) {
+        Ok(articles) => {
+            println!("Found articles...");
+            articles.iter().for_each(|article| println!("article: {:?}", article.url));
+        },
+        Err(error) => eprintln!("Something happened while trying to parse for articles, {:?}", error)
+    };
 }
 
--- src/website.rs
+++ src/website.rs
@@ -1,6 +1,9 @@
+use scraper::{Html, Selector, error::SelectorErrorKind};
 use regex::RegexSet;
 use reqwest;
 
+// this is to get rid of warnings when compiling
+#[allow(dead_code)]
 pub enum SearchTerms {
     Words(Vec<String>),
     Patterns(RegexSet),
@@ -8,32 +11,82 @@ pub enum SearchTerms {
 }
 
 pub struct Website {
-    pub url: String,
-    pub terms_found: i32,
-    pub terms: SearchTerms
+    pub url: String,        // the URL of the website
+    pub terms: SearchTerms, // for filtering out articles
+    pub selector: String    // for scraping articles from the HTTP request 
 }
 
+// this is to get rid of warnings when compiling
+#[allow(dead_code)]
 impl Website {
-    pub fn new() -> Website {
+    /// constructors
+   
+    /// Creates a Website struct from a URL and some search terms
+    pub fn new(url: String, terms: SearchTerms, selector: String) -> Website {
         Website {
-            url: String::new(),
-            terms_found: 0,
-            terms: SearchTerms::None
+            url,
+            terms,
+            selector
         }
     }
 
-    pub async fn make_get(&self) -> Result<(), reqwest::Error> {
-        // getting a response from the website
-        let res = reqwest::get(&self.url).await?;
-    
-        println!("Response: {:?} {}", res.version(), res.status());
-        println!("Headers: {:#?}\n", res.headers());
-    
-        let body = res.text().await?;
-    
-        println!("{}", body);
-    
-        Ok(())
+    /// Creates an empty Website struct
+    pub fn empty() -> Website {
+        Self::new(String::new(), SearchTerms::None, String::new())
+    }
+
+    /// Creates a Website struct with a URL
+    pub fn with_url(url: String) -> Website {
+        Self::new(url, SearchTerms::None, String::from("a"))
+    }
+
+    /// Creates a Website struct with a URL and selector
+    pub fn with_url_and_sel(url: String, selector: String) -> Website {
+        Self::new(url, SearchTerms::None, selector)
+    }
+
+    /// getters/setters
+
+    pub fn set_terms(&mut self, new_terms: SearchTerms) {
+        self.terms = new_terms;
+    }
+
+    pub fn set_url(&mut self, new_url: String) {
+        self.url = new_url;
+    }
+
+    /// methods
+
+    pub fn find_articles(&self, body: &String) -> Result<Vec<Website>, SelectorErrorKind> {
+        // creating storage for all the <a> tags found
+        let mut articles = Vec::new();
+
+        // creating a selector to target articles
+        let sel = match Selector::parse(&self.selector) {
+            Ok(s) => s,
+            Err(error) => return Err(error),
+        };
+
+        // casting the body into a document
+        let document = Html::parse_document(&body);
+
+        // parsing the HTML body
+        for element in document.select(&sel) {
+            match element.value().attr("href") {
+                Some(attr) => articles.push(Self::with_url(attr.to_string())),
+                None => eprintln!("Couldn't extract href from this element")
+            }
+        }
+
+        // returning the articles we found
+        Ok(articles)
+    }
+
+    /// this function makes an HTTP GET request to the URL
+    pub async fn make_get(&self) -> Result<String, reqwest::Error> {
+        // makes a GET request to the URL and returns the body of the
+        // response, error otherwise
+        reqwest::get(&self.url).await?.text().await
     }
 }
 
